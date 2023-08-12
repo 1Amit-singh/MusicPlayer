1d61ab47ada72d96fa0e54702afc035aa789778b
Amit Singh <iamitsinghthakur123@gmail.com>
2023-08-12 23:33:59 +0530

    Added some more functionality like play pause and change cover with progress bar with click event

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ let playBtn = document.getElementsByClassName("play-btn");
 var limit = 12;
 let currentMusic = null; // Variable to store the currently playing song
 let tempMusic = null;
+let progressDiv = document.getElementById("progress-div");
+let progress = document.getElementById("progress");
+let rightPlayBtn = document.getElementById("right-play-btn");
+let rightCover = document.getElementById("right-cover");
+
 
 // Function to stop the currently playing song
 let stopMusic = (ele) => {
@@ -13,6 +18,8 @@ let stopMusic = (ele) => {
         let opt = ele.src.split("/").pop().split(".")[0];
         document.querySelector(`.i${opt}`).classList.remove("fa-circle-pause");
         document.querySelector(`.i${opt}`).classList.add("fa-circle-play");
+        rightPlayBtn.classList.add("fa-play");
+        rightPlayBtn.classList.remove("fa-pause");
     }
 };
 
@@ -27,6 +34,8 @@ let playMusic = (musicUrl) => {
         let opt = currentMusic.src.split("/").pop().split(".")[0];
         document.querySelector(`.i${opt}`).classList.remove("fa-circle-pause");
         document.querySelector(`.i${opt}`).classList.add("fa-circle-play");
+        rightPlayBtn.classList.add("fa-play");
+        rightPlayBtn.classList.remove("fa-pause");
         currentMusic = null;
         return;
     }
@@ -43,6 +52,8 @@ let playMusic = (musicUrl) => {
         let opt = tempMusic.src.split("/").pop().split(".")[0];
         document.querySelector(`.i${opt}`).classList.add("fa-circle-pause");
         document.querySelector(`.i${opt}`).classList.remove("fa-circle-play");
+        rightPlayBtn.classList.remove("fa-play");
+        rightPlayBtn.classList.add("fa-pause");
         currentMusic = tempMusic;
         tempMusic = null;
         return;
@@ -52,21 +63,78 @@ let playMusic = (musicUrl) => {
         // console.log(currentMusic + " This is currentMusic Val");
         // Change the play button icon to "pause" when the song starts playing
         let opt = musicUrl.split("/").pop().split(".")[0];
-        // console.log(opt);
         document.querySelector(`.i${opt}`).classList.remove("fa-circle-play");
         document.querySelector(`.i${opt}`).classList.add("fa-circle-pause");
-        // console.log(currentMusic);
+        rightPlayBtn.classList.remove("fa-play");
+        rightPlayBtn.classList.add("fa-pause");
+        changeCover(currentMusic);
+
+        if (currentMusic) {
+
+
+            progressDiv.onclick = function (e) {
+            console.log((e.offsetX / progressDiv.offsetWidth) * currentMusic.duration);
+            currentMusic.currentTime = (e.offsetX / progressDiv.offsetWidth) * currentMusic.duration;
+            };
+
+            currentMusic.ontimeupdate = function () {
+            let percent = (currentMusic !== null) && ((100 * currentMusic.currentTime) / currentMusic.duration);
+            progress.style.width = percent + "%";
+            };
+        }
+
 
         setTimeout(function(){
             stopMusic(currentMusic);
         },180000);
     }
 
+
+    let changeCover = (currentMusic) => {
+        if(currentMusic){
+            let num = currentMusic.src.split("/").pop().split(".")[0];
+            rightCover.src = `assets/Photos/${num}.png`; 
+            // console.log(rightCover, num)
+        }
+    }
+
+    rightPlayBtn.addEventListener("click", () => {
+        if (currentMusic) {
+            if (currentMusic.paused) {
+                currentMusic.play();
+            } 
+            else {
+                currentMusic.pause();
+            }
+        }
+        else{
+            currentMusic = new Audio("assets/Musics/1.mp3");
+            console.log(currentMusic)
+            currentMusic.play();
+        }
+        updatePlayPauseIcon();
+    });
+    
+    // Function to update the play/pause icon based on audio playback state
+    function updatePlayPauseIcon() {
+        if (currentMusic) {
+            if (currentMusic.paused) {
+                rightPlayBtn.classList.remove("fa-pause");
+                rightPlayBtn.classList.add("fa-play");
+            } else {
+                rightPlayBtn.classList.remove("fa-play");
+                rightPlayBtn.classList.add("fa-pause");
+            }
+        }
+    }   
+
+
+
 // Loop through each song and add event listeners
 for (let i = 0; i < limit; i++) {
     let option = song[i].classList[1].split("s")[1];
     // console.log(option);
-    let musicUrl = `assets/Musics/${option}.mp3`;
+    var musicUrl = `assets/Musics/${option}.mp3`;
 
     // Use a closure to capture the current value of "musicUrl"
     (function (url) {
@@ -82,4 +150,6 @@ for (let i = 0; i < limit; i++) {
     song[i].addEventListener("mouseout", () => {
         playBtn[i].style.display = "none";
     });
+
+    
 }
